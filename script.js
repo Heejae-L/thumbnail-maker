5c16d8294ac201cb07cf912d27be8bf34f853c02
Heejae-L <leeheejae@gachon.ac.kr>
2024-08-01 12:27:57 +0900

    modify design and README.md

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let imageLoaded = false; // 이미지가 로드되었는지 추적하는 플래그
+
+document.getElementById('imageUpload').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    if (!file) {
+        imageLoaded = false; // 파일이 선택되지 않았음
+        return;
+    }
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const img = new Image();
+        img.onload = function() {
+            const canvas = document.getElementById('canvas');
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            imageLoaded = true; // 이미지 로드 완료
+            drawThumbnail(); // 이미지 로드 후 그리기 함수 호출
+        };
+        img.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+});
+
 function drawThumbnail() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
@@ -7,25 +30,25 @@ function drawThumbnail() {
     const text2 = document.getElementById('main-title').value;
     const emoji = document.getElementById('emoji').value;
 
-    // Set background color
-    ctx.fillStyle = bgColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // 이미지가 로드되지 않았을 경우에만 배경색 설정
+    if (!imageLoaded) {
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 
-    // Draw sub-title text at the top left
-    ctx.font = '40px Arial';
+    // 텍스트 및 이모지 그리기
+    ctx.font = '35px Arial';
     ctx.fillStyle = txtColor;
     ctx.textAlign = 'left';
-    ctx.fillText(text1, 20, 50); // Adjusted position to 20 pixels from left, 50 pixels from top
+    ctx.fillText(text1, 30, 60);
 
-    // Draw main-title text centered
     ctx.font = '80px Arial';
     ctx.fillStyle = txtColor;
     ctx.textAlign = 'center';
-    ctx.fillText(text2, canvas.width / 2, canvas.height / 2 + 20); // Centered horizontally, adjust vertically as needed
+    ctx.fillText(text2, canvas.width / 2, canvas.height / 2 + 40);
 
-    // Draw emoji at the left middle
-    ctx.font = '80px Arial';  // Adjust the emoji size if needed
-    ctx.fillText(emoji, 40, canvas.height / 2+20);  // Position emoji on the left, halfway down the canvas
+    ctx.font = '40px Arial';
+    ctx.fillText(emoji, 20, canvas.height / 2);
 }
 
 
